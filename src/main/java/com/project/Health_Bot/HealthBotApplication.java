5a0ffcf27a7997f86420bebbb25fb471c07b74c9
fedePalazz <s1087932@studenti.univpm.it>
2020-12-03 22:44:00 +0100

    Primo commit

---
+++ src/main/java/com/project/Health_Bot/HealthBotApplication.java
@@ -0,0 +1,13 @@
+package com.project.Health_Bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/Health_Bot/HealthBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Health_Bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
