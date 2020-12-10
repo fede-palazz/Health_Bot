1d253c13860ec00b5cf2ac551384472f6fe8b652
GiovanniNovelli9 <s1087129@studenti.univpm.it>
2020-12-10 18:04:58 +0100

    commenti

--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -1,6 +1,7 @@
 package model;
 /*
- * 
+ * questa classe è astratta poichè deve modellare 3 possibili stili di vita.
+ * per ogni stile di vita ho dei metodi in comune ma diversi tra loro -> uso abstract 
  * 
  */
 public abstract class Utente {
