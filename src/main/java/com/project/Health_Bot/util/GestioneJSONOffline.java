8b076e0e9d36d23a28f28e462c7d427246f08130
GiovanniNovelli9 <s1087129@studenti.univpm.it>
2020-12-20 17:54:47 +0100

    metodi JSONArray

--- src/main/java/com/project/Health_Bot/util/GestioneJSONOffline.java
+++ src/main/java/com/project/Health_Bot/util/GestioneJSONOffline.java
@@ -3,10 +3,11 @@
  */
 package com.project.Health_Bot.util;
 
-
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -14,67 +15,113 @@ import org.json.simple.parser.ParseException;
 public class GestioneJSONOffline {
 
 	private static FileWriter file;
-	
-	
-   /**
-    * Metodo che salva un JSONObject in un file di testo .json.
-    * @param nome_file 
-    * @param obj
-    */
-    public static void salvaFile(String nome_file, JSONObject obj) {
-        
+
+	/**
+	 * Metodo che salva un JSONObject in un file di testo .json.
+	 * 
+	 * @param nome_file
+	 * @param obj
+	 */
+	public static void salvaOBJFile(String nome_file, JSONObject obj) {
+
 		try {
-			    //true = non sovrascive il file
-	            file = new FileWriter(nome_file, true); 
-	            file.write(obj.toJSONString());
-	            verifica("Successfully Copied JSON Object to File...");
-	            verifica("\nJSON Object: " + obj);
-	 
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	 
-	        } finally {
-	 
-	            try {
-	                file.flush();
-	                file.close();
-	            } catch (IOException e) {
-	                e.printStackTrace();
-	            }
-	        }
-	    }
-	 
-    
-    /**
-     * Metodo per verificare che il file sia stato salvato correttamente.
-     * @param str
-     */
-    static public void verifica(String str) {
-        System.out.println("str");
-    }
-	
+			// true = non sovrascive il file
+			file = new FileWriter(nome_file, true);
+			file.write(obj.toJSONString());
+			verifica("Successfully Copied JSON Object to File...");
+			verifica("\nJSON Object: " + obj);
 
-   /**
-    * Metodo per caricare un JSONObject salvato in un file locale.
-    * @param nome_file
-    * @return
-    */
-    
-	    public static JSONObject caricaFile (String nome_file) {
-	    	//caricare JSONObject salvato in locale su un file .JSON
-	        JSONParser parser = new JSONParser();
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		} finally {
+
 			try {
-				Object obj = parser.parse(new FileReader(nome_file));
-				JSONObject jsonObject = (JSONObject) obj;
-				return jsonObject;
-	        
-	        }catch (IOException e) {
-	            e.printStackTrace();
-	        } catch (ParseException e) {
-	            e.printStackTrace();
-	        }
-			return null; 
-	    	
-	    }
+				file.flush();
+				file.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
+	/**
+	 * Metodo per verificare che il file sia stato salvato correttamente.
+	 * 
+	 * @param str
+	 */
+	static public void verifica(String str) {
+		System.out.println("str");
+	}
+
+	/**
+	 * Metodo per caricare un JSONObject salvato in un file locale.
+	 * 
+	 * @param nome_file
+	 * @return
+	 */
+
+	public static JSONObject caricaOBJFile(String nome_file) {
+		// caricare JSONObject salvato in locale su un file .JSON
+		JSONParser parser = new JSONParser();
+		try {
+			Object obj = parser.parse(new FileReader(nome_file));
+			JSONObject jsonObject = (JSONObject) obj;
+			return jsonObject;
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+
+	public static void salvaARRAYFile(String nome_file, JSONArray ja) {
+
+		try {
+			// true = non sovrascive il file
+			file = new FileWriter(nome_file, true);
+			file.write(ja.toJSONString());
+			verifica("Successfully Copied JSON Array to File...");
+			verifica("\nJSONArray: " + ja);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		} finally {
+
+			try {
+				file.flush();
+				file.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Metodo per caricare un JSONArray salvato in un file locale.
+	 * 
+	 * @param nome_file
+	 * @return JSONArray
+	 */
+
+	public static JSONArray caricaARRAYFile(String nome_file) {
+		// caricare JSONObject salvato in locale su un file .JSON
+		JSONParser parser = new JSONParser();
+		try {
+			Object obj = parser.parse(new FileReader(nome_file));
+			JSONArray ja = (JSONArray) obj;
+			return ja;
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return null;
+
+	}
 }
