54a42ef39dea1e7be32a9dd71150b7e898b5eba3
GiovanniNovelli9 <s1087129@studenti.univpm.it>
2020-12-29 16:12:23 +0100

    aggiunta package.info in tutti i package

---
+++ src/main/java/com/project/Health_Bot/model/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * TODO
+ * Questo package permette al client di fare chiamate attraverso il Bot.
+ */
+
+/**
+ *  @author FedePalaz & GiovanniNovelli9 & Baldellaux
+ *
+ */
+package com.project.Health_Bot.model;
