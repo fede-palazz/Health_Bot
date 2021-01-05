bd7cfa950ae0060c1d31c1a53b89ebe2bd4e612a
GiovanniNovelli9 <s1087129@studenti.univpm.it>
2021-01-05 11:47:22 +0100

    modifiche JUNIT e readMe

--- src/test/java/com/project/Health_Bot/JSONOfflineTest.java
+++ src/test/java/com/project/Health_Bot/JSONOfflineTest.java
@@ -1,4 +1,4 @@
-package com.project.Health_Bot.JSONOffline;
+package com.project.Health_Bot;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -9,15 +9,20 @@ import static org.junit.Assert.assertTrue;
 import com.project.Health_Bot.model.Utente;
 import com.project.Health_Bot.util.JSONOffline;
 
-
 import org.junit.jupiter.api.*;
 
+/**
+ * 
+ * @author FedePalaz & GiovanniNovelli9 & Baldellaux
+ *
+ */
 public class JSONOfflineTest {
 
 	private String id;
 	private Utente utente;
 	private String tipo;
 	private int num_all;
+
 	/**
 	 * Inizializza i componenti necessari a testare i metodi.
 	 * 
@@ -25,14 +30,15 @@ public class JSONOfflineTest {
 	 */
 	@BeforeEach
 	void setUp() throws Exception {
-		id="1234";
+		id = "1234";
 		tipo = "sed";
 		num_all = 1;
 	}
-	
+
 	/**
-	 * Serve per distruggere ciò che è stato inizializzato dal metodo setUp. throws
-	 * java.lang.Exception
+	 * Serve per distruggere ciò che è stato inizializzato dal metodo setUp.
+	 * 
+	 * @throws java.lang.Exception
 	 */
 	@AfterEach
 	void tearDown() throws Exception {
@@ -41,21 +47,20 @@ public class JSONOfflineTest {
 	/**
 	 * metodi da testare
 	 */
-
 	@Test
 	@DisplayName("Verifica se Utente è registrato")
-	public void tesIsRegistred() {
+	public void tesIsRegistered() {
 		boolean reg = JSONOffline.isRegistered(id);
 		assertFalse(reg);
 	}
-	
+
 	@Test
 	@DisplayName("Verifica se Utente è nullo")
 	public void testGetUtente() {
 		utente = JSONOffline.getUtente(id);
-		assertNotNull(utente);
+		assertNull(utente);
 	}
-	
+
 	@Test
 	@DisplayName("Verifica se l'allenamento restituito non è nullo")
 	public void testGetAllenamento() {
